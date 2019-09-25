97d617542a349a977775c5a804097100c90daeb3
Gerrit Reinke <gre@MacBook-GRE-GK.fritz.box>
2019-09-25 11:33:01 +0200

    add force param for autopilot

--- ClientManagement/GetAutopilotHash.ps1
+++ ClientManagement/GetAutopilotHash.ps1
@@ -1,5 +1,5 @@
 mkdir c:\\HWID >> $null
 Set-Location c:\\HWID 
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
-Install-Script -Name Get-WindowsAutoPilotInfo
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
+Install-Script -Name Get-WindowsAutoPilotInfo -Force
 Get-WindowsAutoPilotInfo.ps1 -OutputFile AutoPilotHWID.csv
